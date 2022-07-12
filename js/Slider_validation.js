327cd9850a4c94adc3a04d7eb46276e92e96084b
lmm_glb <2509819080_glb@qq.com>
2022-07-12 16:43:08 +0800

    Slider validation

---
+++ js/Slider_validation.js
@@ -0,0 +1,56 @@
+window.onload = function () {
+  let dragContainer = document.querySelector('#dragContainer')
+  let dragBg = document.querySelector('#dragBg')
+  let dragText = document.querySelector('#dragText')
+  let dragHandler = document.querySelector('#dragHandler')
+  // 滑块最大偏移量 = 滑块验证容器长度 - 滑块长度
+  let maxHandlerOffset = dragContainer.clientWidth - dragHandler.clientWidth
+  // 是否验证成功标记
+  let isVertifySuccess = false
+  initDrag()
+  function initDrag() {
+    dragText.textContent = '拖动滑块验证'
+    dragHandler.addEventListener('mousedown', onDragHandlerDown)
+  }
+
+  function onDragHandlerDown() {
+    document.addEventListener('mousemove', onDragHandlerMove)
+    document.addEventListener('mouseup', onDragHandlerUp)
+  }
+
+  function onDragHandlerMove(e) {
+    let left = e.clientX - dragHandler.clientWidth / 2
+    if (left < 0) {
+      left = 0
+    } else if (left > maxHandlerOffset) {
+      left = maxHandlerOffset
+    }
+    dragHandler.style.left = left + 'px'
+    dragBg.style.width = dragHandler.style.left
+  }
+
+  function onDragHandlerUp(e) {
+    document.removeEventListener('mousemove', onDragHandlerMove)
+    let left = e.clientX - dragHandler.clientWidth / 2
+    if (left >= maxHandlerOffset) {
+      vertifySuccess()
+    } else {
+      // 初始化滑块移动量
+      dragHandler.style.left = 0
+      dragBg.style.width = 0
+    }
+  }
+
+  // 验证成功
+  function vertifySuccess() {
+    isVertifySuccess = true
+    dragText.textContent = '验证通过'
+    dragText.style.color = 'white'
+    dragHandler.setAttribute('class', 'dargHandlerOkBg')
+    dragHandler.removeEventListener('mousedown', onDragHandlerDown)
+    document.removeEventListener('mousemove', onDragHandlerMove)
+    document.removeEventListener('mouseup', onDragHandlerUp)
+    window.location.href =
+      'https://blog.csdn.net/m0_53375764?spm=1000.2115.3001.5343'
+  }
+}
